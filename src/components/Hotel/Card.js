d950beeb578991afa519637d700cb0b04c663862
Tiago <tiagoalex0202@gmail.com>
2023-08-03 20:24:41 +0100

    Hotel Cards strucuture finished

--- src/components/Hotel/Card.js
+++ src/components/Hotel/Card.js
@@ -1,10 +1,24 @@
 
 import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
 
 export default function Card(hotelInfo) {
+  let available=0;
+
+  for (let i=0; i < hotelInfo.rooms.length; i++) {
+    available = available + hotelInfo.rooms[i].capacity;
+  }
   return (
     <Container>
-      <Image src={hotelInfo.image}/>
+      <Image src={hotelInfo.image} alt="imagem"/>
+      <Name>{hotelInfo.name}</Name>
+      <Details>Tipo de Acomodação:
+        <div>{hotelInfo.rooms.map((i) => `${i.name}, `)}</div>
+      </Details>
+      <Details>Quantidade de Vagas:
+        <div>{available}</div>
+      </Details>
+      
     </Container>
   );
 }
@@ -24,4 +38,19 @@ width:50px;
 height:30px;
 border-radius: 8px;
 margin-top: 5px;`;
+
+const Name= styled.div `
+height: 15px;
+weight:700;
+font-size: 25px`;
+
+const Details= styled.div `
+weight:700;
+height: 10px;
+div {
+  margin-top:5px;
+  weight:400
+  height: 7px;
+};
+margin-bottom: 5px;`;
 
--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,16 +1,23 @@
 import Typography from '@material-ui/core/Typography';
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import useHotel from '../../../hooks/api/useHotel';
+import Card from '../../../components/Hotel/Card';
 
 export default function Hotel() {
-  const [hotels, setHotels] = useState([]);
+  const [hoteis, setHoteis] = useState([]);
+  const { hotels } = useHotel();
 
   useEffect( () => {
+    if(hotels) {
+      setHoteis(hotels);
+    }
+  }, [hotels]);
 
-  }, []);
   return (<div>
     <StyledTypography variant="h4">Escolha de hotel e quarto</StyledTypography>
     <ChooseHotel>Primeiro escolha seu hotel!</ChooseHotel>
+    <CardContainer>{hotels.map((i) => Card(i))}</CardContainer>
   </div>);
 }
 
@@ -21,3 +28,11 @@ const StyledTypography = styled(Typography)`
 const ChooseHotel = styled.div `
 height: 50px;
 color: lightgrey;`;
+
+const CardContainer = styled.div `
+display:flex;
+flex-direction:row;
+justify-content: space-between;
+align-items: center;
+height: 230px;
+margin-top:20px;`;
