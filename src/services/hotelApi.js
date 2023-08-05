576af494a9138bcfedc12a577f6f273a7b6dc38d
Tiago <tiagoalex0202@gmail.com>
2023-08-05 22:29:14 +0100

    makeBooking

--- src/services/hotelApi.js
+++ src/services/hotelApi.js
@@ -20,8 +20,18 @@ export async function getBookingsByRoomId(token, roomId) {
   const response = await api.get(`/booking/${roomId}`, {
     headers: {
       Authorization: `Bearer ${token}`,
-    },
+    }, 
   });
   
   return response.data;
 };
+
+export async function makeBooking(token, roomId) {
+  const response = await api.post('/booking', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }, { roomId });
+
+  return response.data;
+}
